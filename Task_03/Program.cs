ea6821b4209ae38ea0a2689c116b07dfbeeef645
Anastasiya <anastasiya.sachuk@mail.ru>
2023-01-26 21:09:43 +0300

    Add Task 030

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -2,3 +2,23 @@
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 //6, 1, 33 -> [6, 1, 33]
 
+string[] myArray(string step)
+{
+    string[] elements = new string [8];
+    elements = step.Split(",");
+    return elements;
+}
+
+void divide(string[] elements)
+{
+    Console.WriteLine(String.Join(", ", elements));  
+}
+
+void main()
+{
+    Console.WriteLine("Введите 8 чисел через запятую: ");
+    string num = Console.ReadLine()!;
+    divide(myArray(num));
+}
+
+main();
