316d52f64ab17a015568a5b3f83e8c8bdfc03615
Anastasiya <anastasiya.sachuk@mail.ru>
2023-01-27 00:17:18 +0300

    Add Task_4

--- Task_04/Program.cs
+++ Task_04/Program.cs
@@ -1,26 +1,37 @@
 ﻿/* Задача “со звездочкой”: Напишите функцию, которая принимает одно число - высоту елочки и 
 рисует ее в консоли звездочками. (Пример см. презентацию)*/
 Console.Clear();
- Console.WriteLine("Насколько высока елочка? ");
-            int width = int.Parse(Console.ReadLine()!);
-            for (int i = width; i >0; i--)
-            {
-                for (int j = width - i; j<width; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (int j = 0; j < width - i; j++)
-                {
-                    Console.Write("*");
-                }
-                for (int j = 0; j < width - i; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
-            for (int i = width-1; i > 0; i--)
-            {
-                Console.Write(" ");
-            } 
-            Console.ReadKey();
+
+Console.WriteLine("Введите желаемую высоту елочки: ");
+int height = int.Parse(Console.ReadLine()!);
+
+
+for (int i = height; i >= 0; i--)
+    {
+        
+        for (int j = height - i; j < height; j++)
+        {
+            Console.Write(" ");
+        }
+        if (i == height)
+        {
+            Console.WriteLine("*");
+            continue;
+        }
+        for (int j = 0; j < height - i; j++)
+        {
+            Console.Write("*");
+        }
+        Console.Write("*");
+        for (int j = 0; j < height - i; j++)
+        {
+            Console.Write("*");
+        }
+            Console.WriteLine();
+    }
+    for (int i = height; i > 0; i--)
+    {
+        Console.Write(" ");
+    } 
+Console.ReadKey(); 
+
